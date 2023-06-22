fbc7956bf11b89956df630abf9437628419dfb1d
ngerdzhikov <ngerdzhikov@nemetschek.bg>
2023-06-22 17:43:08 +0300

    update with test information

--- pdftronExampleSwift/ViewController.swift
+++ pdftronExampleSwift/ViewController.swift
@@ -1,34 +1,54 @@
-//
-//  ViewController.swift
-//  pdftronExampleSwift
-//
-//  Created by Mark Holland on 18/3/21.
-//
-
 import UIKit
-import PDFNet
 import Tools
 
 class ViewController: UIViewController {
-
+    
+    private var padding = 0
+    private let documentController = PTDocumentController()
+    private var initialZoom = 0.0
+    private var bottom = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-        // Create a PTDocumentController
-        let documentController = PTDocumentController()
-        // The PTDocumentController must be in a navigation controller before a document can be opened
         let navigationController = UINavigationController(rootViewController: documentController)
         navigationController.modalPresentationStyle = .fullScreen
         navigationController.navigationBar.isTranslucent = false
         navigationController.toolbar.isTranslucent = false
-        // Open a file from URL.
-        let fileURL: URL = URL(string:"https://pdftron.s3.amazonaws.com/downloads/pdfref.pdf")!
-        documentController.openDocument(with: fileURL)
-        // Show navigation (and document) controller.
-        self.present(navigationController, animated: true, completion: nil)
+        navigationController.navigationBar.topItem?.leftBarButtonItems = [UIBarButtonItem(title: "Zoom", style: .plain, target: self, action: #selector(test))]
+        
+//        let fileURL: URL = URL(string:"https://pdftron.s3.amazonaws.com/downloads/pdfref.pdf")!
+        let fileURL = Bundle.main.url(forResource: "test2", withExtension: "pdf")
+        documentController.openDocument(with: fileURL!)
+        
+        initialZoom = documentController.pdfViewCtrl.zoom
+        
+        self.present(navigationController, animated: true) {
+            self.enablePadding()
+        }
+        
+    }
+    
+    @objc func test() {
+        var pdfRect: PTPDFRect!
+        if bottom {
+            pdfRect = PTPDFRect(x1: 0, y1: 0, x2: 224, y2: 233)
+        } else {
+            pdfRect = PTPDFRect(x1: 2800, y1: 3800, x2: 3024, y2: 4033)
+        }
+        let cgRect = documentController.pdfViewCtrl.pdfRectPage2CGRectScreen(pdfRect, pageNumber: 1)
+        documentController.pdfViewCtrl.contentScrollView.zoom(to: cgRect, animated: true)
+        bottom = !bottom
+    }
+    
+    private func enablePadding() {
+        let horizontalPadding = 1000.0
+        let verticalPadding = 800.0
+        documentController.pdfViewCtrl.contentScrollView.contentInset = UIEdgeInsets(top: verticalPadding / 2, left: horizontalPadding / 2,
+                                                                                     bottom: verticalPadding / 2, right: horizontalPadding / 2)
+        
     }
 }
-
