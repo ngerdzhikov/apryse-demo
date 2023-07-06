29a29aeb5183be92a341749e1d0f22168fb67998
ngerdzhikov <ngerdzhikov@nemetschek.bg>
2023-07-06 11:48:51 +0300

    an approach with setPageSpacing

--- pdftronExampleSwift/ViewController.swift
+++ pdftronExampleSwift/ViewController.swift
@@ -22,27 +22,25 @@ class ViewController: UIViewController {
         
         let fileURL = Bundle.main.url(forResource: "test2", withExtension: "pdf")
         documentController.openDocument(with: fileURL!)
+        
+        self.present(navigationController, animated: true)
     }
     
     @objc func zoomRect() {
         enablePadding()
-        // if you uncomment the next line it behaves a little better but still not perfectly
-//        documentController.pdfViewCtrl.contentScrollView.zoom(to: documentController.pdfViewCtrl.frame, animated: false)
         var pdfRect = PTPDFRect(x1: -266.91396, y1: 350.44244, x2: 352.46744, y2: 619.9053)!
-        let cgRect = documentController.pdfViewCtrl.pdfRectPage2CGRectScreen(pdfRect, pageNumber: 1)
-        documentController.pdfViewCtrl.contentScrollView.zoom(to: cgRect, animated: true)
+        documentController.pdfViewCtrl.showRect(1, rect: pdfRect)
     }
     
     private func enablePadding() {
         let horizontalPadding = 1000.0
         let verticalPadding = 800.0
-        documentController.pdfViewCtrl.contentScrollView.contentInset = UIEdgeInsets(top: verticalPadding / 2, left: horizontalPadding / 2,
-                                                                                     bottom: verticalPadding / 2, right: horizontalPadding / 2)
+        documentController.pdfViewCtrl.setPageSpacing(0, vert_col_space: 0,
+                                                      horiz_pad: Int32(horizontalPadding) / 2,
+                                                      vert_pad: Int32(verticalPadding) / 2)
     }
     
     @objc func disablePadding() {
-        documentController.pdfViewCtrl.contentScrollView.contentInset = .zero
+        documentController.pdfViewCtrl.setPageSpacing(0, vert_col_space: 0, horiz_pad: 0, vert_pad: 0)
     }
-    
-    
 }
